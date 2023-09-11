fcc7cc1cbb83d0fd5daa13fd7eebe2f816a6bd97
hostr777 <divakovbogdan11@gmail.com>
2023-09-11 11:25:55 +0300

    Update

--- js/CodeSandbox.js
+++ js/CodeSandbox.js
@@ -5,10 +5,4 @@ var swiper = new Swiper(".swiper", {
 	slidesPerColumn: 1,
 	dots: false,
 	keyboard: true,
-	// breakpoints: {
-	// 	630: {
-	// 		spaceBetween: 20,
-	// 		mousewheel: false
-	// 	}
-	// }
   });
